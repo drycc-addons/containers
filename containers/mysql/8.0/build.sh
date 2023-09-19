76997754ae8f146f0792b94879a50bfa533a2e0a
EamonZhang <zhang.eamon@hotmail.com>
2023-09-19 09:05:50 +0800

    chore(containers): add mysql

---
+++ containers/mysql/8.0/build.sh
@@ -0,0 +1,1 @@
+sudo docker build --build-arg CODENAME=bookworm -t registry.drycc.cc/drycc-addons/mysql:8.0.34 --no-cache .
---
+++ containers/mysql/8.0/rootfs/opt/drycc/scripts/mysql/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Copyright VMware, Inc.
+# SPDX-License-Identifier: APACHE-2.0
+
+# shellcheck disable=SC1091
+
+set -o errexit
+set -o nounset
+set -o pipefail
+# set -o xtrace # Uncomment this line for debugging purposes
+
+# Load libraries
+. /opt/drycc/scripts/libdrycc.sh
+. /opt/drycc/scripts/libmysql.sh
+
+# Load MySQL environment variables
+. /opt/drycc/scripts/mysql-env.sh
+
+print_welcome_page
+
+if [[ "$1" = "/opt/drycc/scripts/mysql/run.sh" ]]; then
+    info "** Starting MySQL setup **"
+    /opt/drycc/scripts/mysql/setup.sh
+    info "** MySQL setup finished! **"
+fi
+
+echo ""
+exec "$@"
